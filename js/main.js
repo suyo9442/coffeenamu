1d0bfc1922c165f4a7805099544dbf5b10c0c3a0
suyo9442 <72788139+suyo9442@users.noreply.github.com>
2020-10-30 09:53:21 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,135 @@
-$(function(){
+
+
+$(function () {
+    // var img_num = 0; // 이미지 번호
+    // var duration = 3000; // 인터벌 시간
+    // var slider;   // slider 실행 객체 변수
+  
+    // // 초기화
+    // $('.slider > img').fadeOut();
+    // $('.slider > img').eq(img_num).fadeIn();
+  
+    // // 
+    // function changeSlider() {
+    //   // 전체 이미지 fadeOut
+    //   $('.slider > img').fadeOut();
+    //   // 현재 이미지 fadeIn
+    //   $('.slider > img').eq(img_num).fadeIn();
+    //   // pager UI 업데이트
+    //   $('.pager > a').removeClass('active');
+    //   $('.pager > a').eq(img_num).addClass('active');
+    // }
+  
+
+    // // 슬라이더 재생
+    // function sliderStart() {
+    //   slider = setInterval(function () {
+    //     // 마지막 이미지 번호이면 처음으로 
+    //     if (img_num >= 2) { img_num = 0 } else {
+    //       // 다음 이미지 번호로
+    //       img_num = img_num + 1;
+    //     }
+    //     changeSlider(img_num);
+    //   }, duration);
+    // }
+  
+    // // 슬라이더 정지
+    // function sliderStop() {
+    //   clearInterval(slider);
+    //   console.log('stop');
+    // }
+  
+    // sliderStart();
+  
+    // // 마우스가 올라가면 슬라이더 정지
+    // $('.bxslider').hover(
+    //   function () {
+    //     sliderStop();
+    //   },
+    //   function () {
+    //     sliderStart();
+    //     console.log('start');
+    //   }
+    // )
+  
+    // // 이전 이미지 버튼
+    // $('.controls > .left').click(function () {
+    //   if(img_num > 0) {
+    //     img_num = img_num - 1
+    //   } else { img_num = 2; }
+    //   console.log(img_num);
+    //   changeSlider();
+    // })
+  
+    // // 다음 이미지 버튼
+    // $('.controls > .right').click(function () {
+    //   if(img_num < 2) {
+    //     img_num = img_num + 1;
+    //   } else { img_num = 0; }
+    //   console.log(img_num);
+    //   changeSlider();
+    // })
+  
     
-    var left = 0;
-    var duration = 4000;
+    $('.depth1 > li').hover(
+        function() {
+            $('.accent').css('display','block');
+        },
 
-    setInterval(function(){
-        if(left <= -3840) {
-            left = 0;
+        function() {
+            $('.accent').css('display','none');
         }
+    )
 
-        else {
-            left = left - 1920;
+    $('.depth1 > li:nth-child(1)').hover(
+        function() {
+            $('.accent').css('left','278px');
+        },
+
+        function() {
+            $('.accent').css('display','none');
         }
+    )
+    
+    $('.depth1 > li:nth-child(2)').hover(
+        function() {
+            $('.accent').css('left','389px');
+        },
 
-        $('#banner > .slider').css('marginLeft', left);
+        function() {
+            $('.accent').css('display','none');
+        }
+    )
 
-    }, duration)
-})
+    $('.depth1 > li:nth-child(3)').hover(
+        function() {
+            $('.accent').css('left','503px');
+        },
+
+        function() {
+            $('.accent').css('display','none');
+        }
+    )
+
+    $('.depth1 > li:nth-child(4)').hover(
+        function() {
+            $('.accent').css('left','630px');
+        },
+
+        function() {
+            $('.accent').css('display','none');
+        }
+    )
+    
+    $('.depth1 > li:nth-child(5)').hover(
+        function() {
+            $('.accent').css('left','784px');
+        },
+
+        function() {
+            $('.accent').css('display','none');
+        }
+    )
+
+
+  }) // $end
